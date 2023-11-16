bc1c11a633afa46ddf7586b18f9815ef3276fe59
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2023-11-16 15:57:01 +0500

    hero to'g'irlandiiii

--- src/pages/MensPage.jsx
+++ src/pages/MensPage.jsx
@@ -3,17 +3,19 @@ import "../pages/MensPage.css";
 import Men from "../assets/images/header-side-boy.png";
 const MensPage = () => {
   return (
-    <div className="App">
-      <div className="side-img">
-        <img src={Men} alt="Side Image" />
-      </div>
-      <div className="content">
-        <h1>Универсальное решение для ваших нужд в одежде</h1>
-        <p>
-          Все, что вам нужно, чтобы найти идеальный предмет одежды, есть в Lode
-          X. Мы предлагаем одежду ведущих брендов и редактирование изображений в
-          ней.
-        </p>
+    <div className="container">
+      <div className="App">
+        <div className="side-img">
+          <img src={Men} alt="Side Image" />
+        </div>
+        <div className="content">
+          <h1>Универсальное решение для ваших нужд в одежде</h1>
+          <p>
+            Все, что вам нужно, чтобы найти идеальный предмет одежды, есть в
+            Lode X. Мы предлагаем одежду ведущих брендов и редактирование
+            изображений в ней.
+          </p>
+        </div>
       </div>
     </div>
   );
